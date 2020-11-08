5cd33a28148c752dda909f73a349075b4f874191
guevara-e <71852653+guevara-e@users.noreply.github.com>
2020-11-08 14:38:36 -0600

    Add files via upload

---
+++ src/fuentes/Principal.java
@@ -0,0 +1,21 @@
+
+package fuentes;
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new VentanaP("Titulon");
+//        Ventanita v = new Ventanita();
+//        v.obtenerFuentes();
+                
+        
+        
+        
+    }
+    
+}
